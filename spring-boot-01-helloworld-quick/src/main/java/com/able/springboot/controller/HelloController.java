658d5a4f8384428a531bafbf8699cd4ae4c486fd
jipeng <jipeng@able-elec.com>
2018-04-26 09:35:16 +0800

    springboot快速版

---
+++ spring-boot-01-helloworld-quick/src/main/java/com/able/springboot/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.able.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "你好,宇智波鼬";
+    }
+}
