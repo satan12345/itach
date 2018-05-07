2c6ab77b0c73ab9bbe45b5299c96df5f02ef5812
jipeng <jipeng@able-elec.com>
2018-05-07 13:07:36 +0800

    增加4个注解

---
+++ concurrency/src/main/java/com/able/concurrency/annotion/NotRecommend.java
@@ -0,0 +1,15 @@
+package com.able.concurrency.annotion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 不推荐的写法
+ */
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+    String value() default "";
+}
---
+++ concurrency/src/main/java/com/able/concurrency/annotion/NotThreadSafe.java
@@ -0,0 +1,15 @@
+package com.able.concurrency.annotion;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 课程中用来标记线程不安全的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
